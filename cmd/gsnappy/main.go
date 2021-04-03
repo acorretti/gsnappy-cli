098f3ea395fe6fbd0052fa128c25d1ca1a486658
acorretti <270914+acorretti@users.noreply.github.com>
2021-04-03 20:26:22 +0200

    adding some doc

--- cmd/gsnappy/main.go
+++ cmd/gsnappy/main.go
@@ -1,3 +1,38 @@
+// Copyright 2021 alfonso corretti. All rights reserved.
+// Use of this source code is governed by The MIT License that can be found in
+// the LICENSE file.
+
+/*
+CLI utility for golang/snappy compression & decompression of Snappy block
+format. File and stdio capable.
+
+Compatible with Google C++, Xerial Java and snappyjs JavaScript implementations.
+
+Usage
+
+Print a usage overview with `gsnappy help`
+
+	NAME:
+	gsnappy - Snappy compression utility
+
+	USAGE:
+	gsnappy [global options] command [command options] [arguments...]
+
+	VERSION:
+	1.x
+
+	COMMANDS:
+	c, compress    compress data
+	d, decompress  decompress data
+	help, h        Shows a list of commands or help for one command
+
+	GLOBAL OPTIONS:
+	-i FILE, --input FILE   read input data from FILE (default: stdin)
+	-o FILE, --output FILE  write output data to FILE (default: stdout)
+	--help, -h              show help (default: false)
+	--version, -v           print the version (default: false)
+
+*/
 package main
 
 import (
